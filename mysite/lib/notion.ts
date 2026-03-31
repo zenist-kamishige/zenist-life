25dea1b773da9be88d4efadac3cb0d9523977372
Kamishige <ks.kamishige@gmail.com>
2026-03-31 05:32:34 +0000

    update

--- mysite/lib/notion.ts
+++ mysite/lib/notion.ts
@@ -1,3 +1,47 @@
+import { Client } from "@notionhq/client";
+
+export const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+});
+
+export async function getPosts() {
+  const response = await notion.databases.query({
+    database_id: process.env.NOTION_DATABASE_ID!,
+    page_size: 20,
+    filter: {
+      and: [
+        {
+          property: "Published",
+          checkbox: { equals: true },
+        },
+        {
+          property: "Category",
+          select: { does_not_equal: "セッション" },
+        },
+      ],
+    },
+  });
+  return response.results;
+}
+
+export async function getSessionPage() {
+  const response = await notion.databases.query({
+    database_id: process.env.NOTION_DATABASE_ID!,
+    filter: {
+      and: [
+        {
+          property: "Published",
+          checkbox: { equals: true },
+        },
+        {
+          property: "Category",
+          select: { equals: "セッション" },
+        },
+      ],
+    },
+  });
+  return response.results[0] ?? null;
+}
 export async function getPost(slug: string) {
   const response = await notion.databases.query({
     database_id: process.env.NOTION_DATABASE_ID!,
