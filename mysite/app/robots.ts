b033a131383bddb612ae4ec8fee886ddf6a0a5da
Kamishige <ks.kamishige@gmail.com>
2026-04-01 05:31:49 +0000

    update

---
+++ mysite/app/robots.ts
@@ -0,0 +1,21 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+      {
+        userAgent: 'GPTBot',
+        allow: '/',
+      },
+      {
+        userAgent: 'Claude-Web',
+        allow: '/',
+      },
+    ],
+    sitemap: 'https://zenist-life.net/sitemap.xml',
+  }
+}
