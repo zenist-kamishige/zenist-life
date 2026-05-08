000efecf080b3f4b5c8fa456f6a1ec7377368e79
Kamishige <ks.kamishige@gmail.com>
2026-05-08 07:18:11 +0000

    fix css type declaration

--- mysite/next.config.ts
+++ mysite/next.config.ts
@@ -1,20 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    domains: ["res.cloudinary.com"],
-  },
-};
-
-export default nextConfig;import type { NextConfig } from "next";                                                                                                                                                 
-                                                                                                                                                                                        
-  const nextConfig: NextConfig = {
-    images: {
-      domains: ["res.cloudinary.com"],
-    },                                                                                                                                                                                    
-    async redirects() {
-      return [                                                                                                                                                                            
-        {                                                                                                                                                                               
+ import type { NextConfig } from "next";                                                                                                                                                 
+   
+  const nextConfig: NextConfig = {                                                                                                                                                        
+    images: {                                                                                                                                                                           
+      domains: ["res.cloudinary.com"],                                                                                                                                                    
+    },
+    async redirects() {                                                                                                                                                                   
+      return [                                                                                                                                                                          
+        {
           source: "/:slug((?!posts|about|asobu|nagomu|shira|toku|session|dome).*)",
           destination: "/posts/:slug",                                                                                                                                                    
           permanent: true,
